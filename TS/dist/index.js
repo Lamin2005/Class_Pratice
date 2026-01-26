8fc3c6c84ddf229f771bfe84f109bbe66f88a500
Lamin2005 <dioxidecarbondioxide@gmail.com>
2026-01-26 21:44:41 +0630

    pratice types int TS

--- TS/dist/index.js
+++ TS/dist/index.js
@@ -1,4 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let num = 10;
+num = 20;
+let name = "Mya Mya";
+name = "Mg Mg";
+console.log(name);
+let array = ["lamin", "mgmg", "mama"];
+array.push("aungaung");
+console.log(array);
+let mixed = ['aungaung', 'mgmg', 10, 20];
+mixed.push('laminhein');
+mixed.push(30);
+console.log(mixed);
+let obj = {
+    name: "laminhein",
+    age: 20,
+    adult: true
+};
+obj.name = "aungaung";
+console.log(obj);
 //# sourceMappingURL=index.js.map
--- TS/src/index.ts
+++ TS/src/index.ts
@@ -1,1 +1,34 @@
 let num : number = 10;
+
+num = 20;
+
+let name : string = "Mya Mya";
+
+name = "Mg Mg";
+
+console.log(name);
+
+let array = ["lamin","mgmg","mama"];
+
+array.push("aungaung");
+
+console.log(array);
+
+let mixed = ['aungaung','mgmg',10,20];
+
+mixed.push('laminhein');
+mixed.push(30);
+
+console.log(mixed);
+
+
+let obj = {
+    name : "laminhein",
+    age : 20,
+    adult : true
+}
+
+obj.name = "aungaung";
+
+console.log(obj);
+
